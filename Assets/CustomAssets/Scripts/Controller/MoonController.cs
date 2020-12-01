c2782456c9dc418f63db7c7644e6fc15848e58a8
Gamu2059 <yamagami0711syo@gmail.com>
2020-12-01 16:51:03 +0900

    一通りのUIのガワ実装

---
+++ Assets/CustomAssets/Scripts/Controller/MoonController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonController : MonoBehaviour {
+    [SerializeField]
+    private Transform target;
+
+    private void Update() {
+        var angle = transform.localEulerAngles;
+        angle.x = target.localEulerAngles.x;
+        transform.localEulerAngles = angle;
+    }
+}
