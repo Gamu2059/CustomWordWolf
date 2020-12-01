971950853937898b19552b37c3be64dd24ab03f7
Gamu2059 <yamagami0711syo@gmail.com>
2020-12-02 02:51:58 +0900

    部屋作成UIの実装

---
+++ Assets/CustomAssets/Scripts/Lobby/CreateRoom/CreateRoomView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Common;
+using Cysharp.Threading.Tasks;
+using UI.Button;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lobby.CreateRoom {
+    public class CreateRoomView :
+        MonoBehaviour,
+        Initializable {
+        [SerializeField]
+        private InputField createRoomNameInputField;
+
+        [SerializeField]
+        private CustomButton applyButton;
+
+        [SerializeField]
+        private CustomButton backButton;
+
+        public string RoomName => createRoomNameInputField.text;
+        public IObservable<Unit> ApplyObservable => applyButton.Button.OnClickAsObservable();
+
+        public IObservable<Unit> BackObservable => backButton.Button.OnClickAsObservable();
+
+        public void Initialize() {
+            createRoomNameInputField.OnValueChangedAsObservable().Subscribe(OnEditNameChanged).AddTo(gameObject);
+        }
+
+        public async UniTask ShowAsync() {
+            gameObject.SetActive(true);
+            applyButton.Show();
+            backButton.Show();
+
+            createRoomNameInputField.text = String.Empty;
+        }
+
+        public async UniTask HideAsync() {
+            gameObject.SetActive(false);
+        }
+
+        private void OnEditNameChanged(string roomName) {
+            var isValidName =
+                roomName != null &&
+                !string.IsNullOrEmpty(roomName) &&
+                !string.IsNullOrWhiteSpace(roomName);
+
+            applyButton.Button.interactable = isValidName;
+        }
+    }
+}
