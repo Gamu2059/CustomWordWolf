7e23c4ebbc37e2f159118f0d25dd96584eabc116
Gamu2059 <yamagami0711syo@gmail.com>
2020-12-02 18:38:36 +0900

    追加忘れを追加

---
+++ Assets/CustomAssets/Scripts/Game/Ready/ReadyModel.cs
@@ -0,0 +1,11 @@
+﻿using ConnectData;
+
+namespace Game.Ready {
+    public class ReadyModel {
+        public RoomDetailData RoomData { get; private set; }
+
+        public void SetRoomData(RoomDetailData roomData) {
+            RoomData = roomData;
+        }
+    }
+}
